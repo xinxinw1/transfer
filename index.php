13f45f69e24814a8f0ce32bcfdf58fff1763c852
Xin-Xin Wang - blueberry <xin-xinw@musiclifephilosophy.com>
2016-05-03 21:54:29 -0400

    stop using npm, bump version, add only needed fine-uploader files to git

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
-<?php $ver = "1.1.0"; ?>
+<?php $ver = "2.0.0"; ?>
 <?php require "functions.php"; ?>
 <?php
 check_login();
@@ -42,8 +42,7 @@ if (isset($link)){
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="transfer.css" rel="stylesheet">
-  <link href="node_modules/fine-uploader/fine-uploader/fine-uploader-new.css" rel="stylesheet">
-  <script src="node_modules/fine-uploader/fine-uploader/fine-uploader.js"></script>
+  <link href="fine-uploader/fine-uploader-new.min.css" rel="stylesheet">
 </head>
 
 <body>
@@ -86,93 +85,94 @@ if (isset($link)){
   </div>
   
   <script type="text/template" id="qq-template">
-      <div class="qq-uploader-selector qq-uploader xuploader" qq-drop-area-text="Drop files here">
-          <div class="qq-total-progress-bar-container-selector qq-total-progress-bar-container">
-              <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-total-progress-bar-selector qq-progress-bar qq-total-progress-bar"></div>
-          </div>
-          <div class="qq-upload-drop-area-selector qq-upload-drop-area" qq-hide-dropzone>
-              <span class="qq-upload-drop-area-text-selector"></span>
-          </div>
-          <div class="qq-upload-button-selector qq-upload-button">
-              <div>Upload a file</div>
-          </div>
-              <span class="qq-drop-processing-selector qq-drop-processing">
-                  <span>Processing dropped files...</span>
-                  <span class="qq-drop-processing-spinner-selector qq-drop-processing-spinner"></span>
-              </span>
-          <ul class="qq-upload-list-selector qq-upload-list" aria-live="polite" aria-relevant="additions removals">
-              <li>
-                  <div class="qq-progress-bar-container-selector">
-                      <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-progress-bar-selector qq-progress-bar"></div>
-                  </div>
-                  <span class="qq-upload-spinner-selector qq-upload-spinner"></span>
-                  <span class="qq-upload-file-selector qq-upload-file"></span>
-                  <span class="qq-edit-filename-icon-selector qq-edit-filename-icon" aria-label="Edit filename"></span>
-                  <input class="qq-edit-filename-selector qq-edit-filename" tabindex="0" type="text">
-                  <span class="qq-upload-size-selector qq-upload-size"></span>
-                  <button type="button" class="qq-btn qq-upload-cancel-selector qq-upload-cancel">Cancel</button>
-                  <button type="button" class="qq-btn qq-upload-retry-selector qq-upload-retry">Retry</button>
-                  <button type="button" class="qq-btn qq-upload-delete-selector qq-upload-delete">Delete</button>
-                  <span role="status" class="qq-upload-status-text-selector qq-upload-status-text"></span>
-              </li>
-          </ul>
-
-          <dialog class="qq-alert-dialog-selector">
-              <div class="qq-dialog-message-selector"></div>
-              <div class="qq-dialog-buttons">
-                  <button type="button" class="qq-cancel-button-selector">Close</button>
-              </div>
-          </dialog>
-
-          <dialog class="qq-confirm-dialog-selector">
-              <div class="qq-dialog-message-selector"></div>
-              <div class="qq-dialog-buttons">
-                  <button type="button" class="qq-cancel-button-selector">No</button>
-                  <button type="button" class="qq-ok-button-selector">Yes</button>
-              </div>
-          </dialog>
-
-          <dialog class="qq-prompt-dialog-selector">
-              <div class="qq-dialog-message-selector"></div>
-              <input type="text">
-              <div class="qq-dialog-buttons">
-                  <button type="button" class="qq-cancel-button-selector">Cancel</button>
-                  <button type="button" class="qq-ok-button-selector">Ok</button>
-              </div>
-          </dialog>
+    <div class="qq-uploader-selector qq-uploader xuploader" qq-drop-area-text="Drop files here">
+      <div class="qq-total-progress-bar-container-selector qq-total-progress-bar-container">
+        <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-total-progress-bar-selector qq-progress-bar qq-total-progress-bar"></div>
       </div>
+      <div class="qq-upload-drop-area-selector qq-upload-drop-area" qq-hide-dropzone>
+        <span class="qq-upload-drop-area-text-selector"></span>
+      </div>
+      <div class="qq-upload-button-selector qq-upload-button">
+        <div>Upload a file</div>
+      </div>
+        <span class="qq-drop-processing-selector qq-drop-processing">
+          <span>Processing dropped files...</span>
+          <span class="qq-drop-processing-spinner-selector qq-drop-processing-spinner"></span>
+        </span>
+      <ul class="qq-upload-list-selector qq-upload-list" aria-live="polite" aria-relevant="additions removals">
+        <li>
+          <div class="qq-progress-bar-container-selector">
+            <div role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" class="qq-progress-bar-selector qq-progress-bar"></div>
+          </div>
+          <span class="qq-upload-spinner-selector qq-upload-spinner"></span>
+          <span class="qq-upload-file-selector qq-upload-file"></span>
+          <span class="qq-edit-filename-icon-selector qq-edit-filename-icon" aria-label="Edit filename"></span>
+          <input class="qq-edit-filename-selector qq-edit-filename" tabindex="0" type="text">
+          <span class="qq-upload-size-selector qq-upload-size"></span>
+          <button type="button" class="qq-btn qq-upload-cancel-selector qq-upload-cancel">Cancel</button>
+          <button type="button" class="qq-btn qq-upload-retry-selector qq-upload-retry">Retry</button>
+          <button type="button" class="qq-btn qq-upload-delete-selector qq-upload-delete">Delete</button>
+          <span role="status" class="qq-upload-status-text-selector qq-upload-status-text"></span>
+        </li>
+      </ul>
+
+      <dialog class="qq-alert-dialog-selector">
+        <div class="qq-dialog-message-selector"></div>
+        <div class="qq-dialog-buttons">
+          <button type="button" class="qq-cancel-button-selector">Close</button>
+        </div>
+      </dialog>
+
+      <dialog class="qq-confirm-dialog-selector">
+        <div class="qq-dialog-message-selector"></div>
+        <div class="qq-dialog-buttons">
+          <button type="button" class="qq-cancel-button-selector">No</button>
+          <button type="button" class="qq-ok-button-selector">Yes</button>
+        </div>
+      </dialog>
+
+      <dialog class="qq-prompt-dialog-selector">
+        <div class="qq-dialog-message-selector"></div>
+        <input type="text">
+        <div class="qq-dialog-buttons">
+          <button type="button" class="qq-cancel-button-selector">Cancel</button>
+          <button type="button" class="qq-ok-button-selector">Ok</button>
+        </div>
+      </dialog>
+    </div>
   </script>
   
+  <script src="fine-uploader/fine-uploader.min.js"></script>
   <script>
-        var uploader = new qq.FineUploader({
-            debug: true,
-            element: document.getElementById('fine-uploader'),
-            request: {
-                endpoint: "upload"
-            },
-            chunking: {
-                enabled: true,
-                concurrent: {
-                    enabled: true
-                },
-                success: {
-                    endpoint: "upload?done"
-                }
-            },
-            resume: {
-                enabled: true
-            },
-            retry: {
-                enableAuto: false,
-                showButton: true
-            },
-            callbacks: {
-              onAllComplete: function (success, failed){
-                  if (failed.length == 0)location.replace("?success");
-              }
-            }
-        });
-    </script>
+  var uploader = new qq.FineUploader({
+    debug: true,
+    element: document.getElementById('fine-uploader'),
+    request: {
+      endpoint: "upload"
+    },
+    chunking: {
+      enabled: true,
+      concurrent: {
+          enabled: true
+      },
+      success: {
+          endpoint: "upload?done"
+      }
+    },
+    resume: {
+      enabled: true
+    },
+    retry: {
+      enableAuto: false,
+      showButton: true
+    },
+    callbacks: {
+      onAllComplete: function (success, failed){
+        if (failed.length == 0)location.replace("?success");
+      }
+    }
+  });
+  </script>
 </body>
 
 </html>
--- login.php
+++ login.php
@@ -1,5 +1,5 @@
 <?php session_start(); ?>
-<?php $ver = "1.1.0"; ?>
+<?php $ver = "2.0.0"; ?>
 <?php require "functions.php"; ?>
 <?php
 if (isset($_SESSION['transfer-priv'])){
